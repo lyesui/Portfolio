e736c8abb4e0eadb95e7bf4da30944b6cbb24e46
Lyes Hamidi <hamidilyes22@gmail.com>
2021-01-14 19:09:01 +0100

    :rocket: Added smooth scrolling

--- public/js/app.js
+++ public/js/app.js
@@ -306,3 +306,21 @@ document.addEventListener("DOMContentLoaded", (event) => {
     },
   });
 });
+
+document
+    .querySelectorAll('a[href^="#"]')
+    .forEach(trigger => {
+        trigger.onclick = function(e) {
+            e.preventDefault();
+            let hash = this.getAttribute('href');
+            let target = document.querySelector(hash);
+            let headerOffset = 100;
+            let elementPosition = target.offsetTop;
+            let offsetPosition = elementPosition - headerOffset;
+
+            window.scrollTo({
+                top: offsetPosition,
+                behavior: "smooth"
+            });
+        };
+    });
